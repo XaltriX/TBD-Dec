93838ae1f4afee54e35fda220646e986c57bea29
XaltriX <129171425+XaltriX@users.noreply.github.com>
2024-12-04 14:30:42 +0530

    Update video.py

--- video.py
+++ video.py
@@ -101,7 +101,7 @@ async def upload_video(client, file_path, thumbnail_path, video_title, reply_msg
         collection_message = await client.send_video(
             chat_id=collection_channel_id,
             video=file,
-            caption=f"✨ {video_title}\n👤 ʟᴇᴇᴄʜᴇᴅ ʙʏ : {user_mention}\n⚡ ᴅᴇᴠʟᴏᴘᴇʀ : <a href=https://t.me/HateXfree>ᯓ ʜᴀᴛᴇ ғʀᴇᴇ ᡣ𐭩</a>\nʙʏ : @TeraboXvideossbot",
+            caption=f"✨ {video_title}\n👤 ʟᴇᴇᴄʜᴇᴅ ʙʏ : {user_mention}\n⚡ ᴅᴇᴠʟᴏᴘᴇʀ : <a href=https://t.me/Xaltrix>ᯓ ʜᴀᴛᴇ ғʀᴇᴇ ᡣ𐭩</a>\nʙʏ : @TeraBoxDownload3rBot",
             thumb=thumbnail_path,
             progress=progress
         )
